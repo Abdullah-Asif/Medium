80444e4f868fe7c7505a6150ae535feb21bd2d8a
Asif <asifabdullah135@gmail.com>
2023-12-07 12:27:38 +0600

    initiating database connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from 'express';
 import { userRoute } from './webApi/routes/userRoute';
 import dotenv from 'dotenv';
+import {Database} from "./infrastructure/dbConfig";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT;
+const database = new Database();
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
 });
