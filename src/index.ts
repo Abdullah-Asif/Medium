b38cc42bffee35da30773eedf02bfb6d6edaeb50
Asif <asifabdullah135@gmail.com>
2023-12-08 10:06:35 +0600

    blog route added

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import { userRoute } from './webApi/routes/userRoute';
+import { blogRoute } from "./webApi/routes/blogRoute";
 import dotenv from 'dotenv';
 import {Database} from "./infrastructure/dbConfig";
+import BlogController from "./webApi/controllers/blogController";
 
 dotenv.config();
 
@@ -10,9 +12,12 @@ app.use(express.json());
 const port = process.env.PORT;
 const database = new Database();
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
+    res.send('Hello Pants!!!!');
 });
-app.use('/user', userRoute)
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
+app.get('/users/:username/blogs', BlogController.getBlogsByUserName);
+app.post('/users/:username/blogs', BlogController.createBlog);
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
