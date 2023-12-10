30ea498d3c98c694a753d3dc6a83f1bcedd937be
Asif <asifabdullah135@gmail.com>
2023-12-10 18:51:41 +0600

    route moved from index to route file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { userRoute } from './webApi/routes/userRoute';
 import { blogRoute } from "./webApi/routes/blogRoute";
+import { authRoute} from "./webApi/routes/authRoute";
 import dotenv from 'dotenv';
 import {Database} from "./infrastructure/dbConfig";
 import BlogController from "./webApi/controllers/blogController";
@@ -14,10 +15,14 @@ const database = new Database();
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Pants!!!!');
 });
-app.use('/user', userRoute);
-app.use('/blog', blogRoute);
-app.get('/users/:username/blogs', BlogController.getBlogsByUserName);
-app.post('/users/:username/blogs', BlogController.createBlog);
+app.use('/users', userRoute);
+app.use('/blogs', blogRoute);
+app.use('/auth', authRoute);
+
+//global exception middleware
+//cors
+//rate limiter
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
